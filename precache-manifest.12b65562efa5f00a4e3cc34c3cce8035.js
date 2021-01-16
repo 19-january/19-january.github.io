a21c74ac98b027c5105d1bf7f62efd0558138493
Anton Cherepov <jacobxblue@gmail.com>
2021-01-17 02:56:21 +0300

    feat: аналитика

---
+++ precache-manifest.12b65562efa5f00a4e3cc34c3cce8035.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fa0c74aa862e92b446e4a566ccb0e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "74f4454d6d1e1902576a",
+    "url": "/static/js/2.06e5523d.chunk.js"
+  },
+  {
+    "revision": "64a4a0d3461e81d764505afec889d794",
+    "url": "/static/js/2.06e5523d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ee5ddb6147beddd437",
+    "url": "/static/js/main.6cd81c80.chunk.js"
+  },
+  {
+    "revision": "f35247d38ed1e9a67c28",
+    "url": "/static/js/runtime-main.6fb393f2.js"
+  }
+]);
